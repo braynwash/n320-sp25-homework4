db434113a079ca28a622a33746f9666f5d6368d4
Erin Ballinger <erballin@iu.edu>
2025-03-09 14:26:41 -0400

    Small adjustment to pdf generation

--- views/app/PDFGenerator.js
+++ views/app/PDFGenerator.js
@@ -4,7 +4,7 @@ class PDFGenerator {
   constructor(title = "Document") {
     this.doc = new jsPDF();
     this.title = title;
-    this.fontSize = 18;
+    this.fontSize = 13;
     this.font = "Helvetica";
     this.doc.setFont(this.font, "normal");
     this.doc.setFontSize(this.fontSize);
@@ -18,34 +18,52 @@ class PDFGenerator {
     this.title = title;
   }
 
+  setFontWeight(weight) {
+    this.doc.setFont(this.font, weight);
+  }
+  setFontColor(r, g, b) {
+    this.doc.setTextColor(r, g, b);
+  }
+  resetFontColor() {
+    this.doc.setTextColor(0, 0, 0);
+  }
+  setFontSize(newFontSize) {
+    this.doc.setFontSize(newFontSize);
+  }
+  resetFontSize() {
+    this.doc.setFontSize(this.fontSize);
+  }
+
   addHeader(text) {
     // Draw header text.
-    this.doc.setFontSize(24);
-    this.doc.setFont(this.font, "bold");
-    this.addText(text, 10, null, 10);
+    this.setFontSize(20);
+    this.setFontWeight("bold");
+    this.addText(text, 10, null, 6);
 
     // Draw the <hr>
-    let lineY = this.currentY - 7.5;
-    this.doc.setDrawColor(255, 0, 0);
-    this.doc.setFillColor(255, 0, 0);
+    let lineY = this.currentY + 2;
+    this.doc.setDrawColor(249, 77, 61);
+    this.doc.setFillColor(249, 77, 61);
     this.doc.rect(10, lineY, this.doc.internal.pageSize.getWidth() - 20, 1, "DF");
 
     // Add space after Header
     this.addText("", 10, null, 1);
 
     // Reset font style
-    this.doc.setFontSize(this.fontSize);
-    this.doc.setFont(this.font, "normal");
+    this.setFontWeight("normal");
+    this.resetFontSize();
+    this.doc.setDrawColor(0, 0, 0, 1);
+    this.doc.setFillColor(0, 0, 0, 0);
   }
 
   addText(text, x = 10, y = null, lineHeight = 7) {
-    if (this.currentY == null) this.currentY = 17;
-    this.doc.text(text, x, this.currentY);
+    if (this.currentY == null) this.currentY = 0;
     if (y === null) {
       this.currentY = (this.currentY || 10) + lineHeight;
     } else {
       this.currentY = y;
     }
+    this.doc.text(text, x, this.currentY);
   }
 
   getPdfUrl() {
